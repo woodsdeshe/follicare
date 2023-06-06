67f79333d43042b8aba8a70d40ee3ed52784b554
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-06 09:57:14 -0400

    created register and login method in auth.service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:8080/auth/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(newUserData: any): Observable<any> {
+    const url = `${this.baseUrl}/register`;
+    return this.http.post(url, newUserData);
+  }
+
+  login(credentials: any): Observable<any> {
+    const url = `${this.baseUrl}/login`;
+    return this.http.post(url, credentials);
+  }
 }
