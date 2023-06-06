59b08cc82e39365027a8bee019030f736f4631fb
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-06 13:19:02 -0400

    Added toggleSignUp() to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private sharedService: SharedService) {}
+
+  toggleSignUp() {
+    this.sharedService.toggleSignUp(true);
+  }
 
 }
