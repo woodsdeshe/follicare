9fae7df4b3eeefa078e3c16bc557317d8425cd2b
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-06 07:45:40 -0400

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProfileComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule
