78827574eac978ed71600c0f6e32dd6e7401e03c
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-06 07:49:49 -0400

    Revert "Components created"
    This reverts commit 9fae7df4b3eeefa078e3c16bc557317d8425cd2b.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RegistrationComponent,
-    LoginComponent,
-    ProfileComponent,
-    FavoritesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
