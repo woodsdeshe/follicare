66183e4707f490b2f05a31b258d28e3c8d7bf9bc
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-07 18:33:57 -0400

    Resources search component partial complete

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-favorites',
--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resources',
@@ -8,23 +9,56 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ResourcesComponent implements OnInit {
   resources: any[] = [];
+  partialTitle: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.partialTitle = '';
+  }
+
+
 
   ngOnInit(): void {
-      this.fetchResources();
+    this.route.queryParams.subscribe(params => {
+      this.partialTitle = params['partialTitle'];
+      if (this.partialTitle) {
+        this.fetchResources();
+      } else {
+        this.fetchAllResources();
+      }
+    });
+  }
+  
+
+  fetchAllResources(): void {
+    this.http.get<any[]>('http://localhost:8080/api/resources/all')
+      .subscribe(
+        response => {
+          this.resources = response;
+          console.log(response);
+        },
+        error => {
+          console.error("Couldn't get resources");
+        }
+      );
   }
 
   fetchResources(): void {
-    this.http.get<any[]>('http://localhost:8080/api/resources/all')
-    .subscribe(
-      response => {
-        this.resources = response;
-        console.log(response)
-      },
-      error => {
-        console.error("Couldn't get resources")
-      }
-    )
+    let apiUrl = 'http://localhost:8080/api/resources';
+  
+    if (this.partialTitle && this.partialTitle !== '') {
+      apiUrl += `?partialTitle=${this.partialTitle}`;
+    }
+  
+    this.http.get<any[]>(apiUrl)
+      .subscribe(
+        response => {
+          this.resources = response;
+          console.log(response);
+        },
+        error => {
+          console.error("Couldn't get resources");
+        }
+      );
   }
+  
 }
