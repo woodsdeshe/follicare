8ef59bbc947287e9021efc1b466608ff29b78891
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-07 08:46:05 -0400

    Completed logic for resources component

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.css']
 })
-export class ResourcesComponent {
+export class ResourcesComponent implements OnInit {
+  resources: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+      this.fetchResources();
+  }
+
+  fetchResources(): void {
+    this.http.get<any[]>('http://localhost:8080/api/resources/all')
+    .subscribe(
+      response => {
+        this. resources = response;
+      },
+      error => {
+        console.error("Couldn't get resources")
+      }
+    )
+  }
 }
