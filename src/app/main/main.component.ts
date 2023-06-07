330b932aaac7571cf0595d46c425f65d34e3a1d0
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-06 22:46:58 -0400

    Mian component done

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  content?: string;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getPublicContent().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {console.log(err)
+        if (err.error) {
+          this.content = JSON.parse(err.error).message;
+        } else {
+          this.content = "Error with status: " + err.status;
+        }
+      }
+    });
+  }
 }
