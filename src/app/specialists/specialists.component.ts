0f6dea1be22613516df0441024b9a485f71484c5
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-07 17:03:37 -0400

    Added search functionality to specialist component

--- src/app/specialists/specialists.component.ts
+++ src/app/specialists/specialists.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -9,14 +10,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SpecialistsComponent implements OnInit{
 specialists: any[] = [];
+specialty: string = '';
+zipCode: string = '';
 
-constructor(private http: HttpClient) {}
+constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
 ngOnInit(): void {
-    this.fetchResources();
+    this.route.queryParams.subscribe(params => {
+      this.specialty = params['specialty'];
+      this.zipCode = params['zipCode'];
+      if (this.specialty || this.zipCode) {
+        this.fetchSpecialists();
+      } else {
+        this.fetchAllSpecialists();
+      }
+    });
 }
 
-fetchResources(): void {
+fetchAllSpecialists(): void {
   this.http.get<any[]>('http://localhost:8080/api/specialists')
   .subscribe(
     response => {
@@ -26,7 +37,28 @@ fetchResources(): void {
     error => {
       console.error("Couldn't get specialists")
     }
-    
-  )
-}
+     );
+    }
+
+    fetchSpecialists(): void {
+      let apiUrl = 'http://localhost:8080/api/specialists';
+  
+      if (this.specialty && this.zipCode) {
+        apiUrl += `?specialty=${this.specialty}&zipCode=${this.zipCode}`;
+      } else if (this.specialty) {
+        apiUrl += `?specialty=${this.specialty}`;
+      } else if (this.zipCode) {
+        apiUrl += `?zipCode=${this.zipCode}`;
+      }
+  
+      this.http.get<any[]>(apiUrl).subscribe(
+        response => {
+          this.specialists = response;
+          console.log(response);
+        },
+        error => {
+          console.error("Couldn't get specialists");
+        }
+      );
+    }
 }
