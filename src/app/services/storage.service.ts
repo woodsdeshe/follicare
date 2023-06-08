4c7aa3c33f9222dfb03685b29f11ddef723f3ccf
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-08 19:26:34 -0400

    Service docstrings done

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -8,15 +8,26 @@ const USER_KEY = 'auth-user';
 export class StorageService {
   constructor() {}
 
+  /**
+   * Clears the session storage.
+   */
   clean(): void {
     window.sessionStorage.clear();
   }
 
+  /**
+   * Saves the user data to the session storage.
+   * @param user The user data to be saved
+   */
   public saveUser(user: any): void {
     window.sessionStorage.removeItem(USER_KEY);
     window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
+  /**
+   * Retrieves the user data from the session storage.
+   * @returns The user data, or an empty object if not found
+   */
   public getUser(): any {
     const user = window.sessionStorage.getItem(USER_KEY);
     if (user) {
@@ -26,6 +37,10 @@ export class StorageService {
     return {};
   }
 
+  /**
+   * Checks if a user is logged in by checking the session storage for user data.
+   * @returns True if a user is logged in, false otherwise
+   */
   public isLoggedIn(): boolean {
     const user = window.sessionStorage.getItem(USER_KEY);
     if (user) {
